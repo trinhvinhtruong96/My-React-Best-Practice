9b434c98ee908ad482b46c3092e6c7f9e0c522b0
Trinh Vinh Truong <trinhvinhtruong@gmail.com>
2022-10-14 13:29:43 +0700

    <div>  <a href="https://www.js.education">Click me!</a> </div>

--- babelTranspileExample.js
+++ babelTranspileExample.js
@@ -1,7 +1,22 @@
 
-{/* <img src="https://www.js.education/images/logo.png" alt="JS Education" /> */}
+{/* <img src="https://www.js.education/images/logo.png" alt="JS Education" /> */ }
 _jsx("img", {
   src: "https://www.js.education/images/logo.png",
   alt: "JS Education"
- })
- 
+})
+
+{/*
+<div>
+ <a href="https://www.js.education">Click me!</a>
+</div> 
+*/}
+_jsx(
+  "div",
+  null,
+  _jsx(
+    "a",
+    { href: "https://www.js.education" },
+    "Click me!"
+  )
+)
+
